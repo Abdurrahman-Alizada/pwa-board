aa56a90aef50fd43bb3717996a66454bacb260df
Abdurrahman-Alizada <abdurrahmanalizada17@gmail.com>
2021-12-18 22:55:56 +0500

    sidebar colors fixed

--- components/Home.vue
+++ components/Home.vue
@@ -1,58 +1,111 @@
 <template>
+<!-- navbar -->
   <nav class="flex fixed w-full items-center justify-between h-16 bg-white text-gray-700 border-b border-gray-200 z-10">
-  
-   <!-- navbar -->
-
-      <button class="h-16 flex items-center justify-center w-16 border-r border-gray-400" aria-label="Open Menu" @click="drawer">
+       <div class="flex items-center justify-center">
+        <button class="h-16 flex items-center justify-center w-16 border-r border-gray-400" aria-label="Open Menu" @click="drawer">
 
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"   :d="isOpen ? 'M15 19l-7-7 7-7' : 'M9 5l7 7-7 7'"  />
         </svg>
-                
-        <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
-        </svg> -->
-
-      </button>
-
-      <button  class="h-16 flex items-center justify-center w-16 border-r border-gray-400" aria-label="Open Menu" @click="showdropdown">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </button>
-
-      
-<!-- end navbar -->
-
-<aside class="transform top-0 left-0 w-16 mt-16 bg-gray-600 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30">
-      
-      <span v-for="(item, index) in items" :key="index" @click="selectedItem(index)" class="flex items-center justify-center p-4 text-white hover:bg-green-200 hover:text-green-500 ">
-        <span class="mr-2">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6">
-            <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+        </button>
+         <div class="ml-5 flex text-green-700 font-bold text-xl items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
+          <span class="ml-1">morpheus</span>
+         </div>
+       </div>
+    <button  class="h-16 flex items-center justify-center w-16 border-r border-gray-400" aria-label="Open Menu" @click="showdropdown">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+      </svg>
+    </button>  
+<!-- end navbar -->
+<!-- sidebar -->
+    <aside class="transform top-0 left-0 w-16 mt-16 bg-gray-600 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30">
+      <span v-for="(item, index) in items" :key="index" @click="selectedItem(index)" class="flex items-center justify-center p-4 text-white hover:bg-gray-700 hover:text-green-400 ">
+        <span class="mr-2">
+          <div v-html="item.svg "></div>
         </span>
-      </span>
-        
-    </aside>    
-    <aside class="transform mt-16 ml-16 top-0 left-0 w-32 bg-white border-r border-gray-300 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-0"  
+      </span>        
+    </aside> 
+<!-- second sidebar -->
+    <aside class="transform mt-16 ml-16 top-0 left-0 w-36 bg-gray-700 border-r border-gray-300 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-0"  
      :class="isOpen ? 'translate-x-0' : '-translate-x-full'" 
      >
      <Sidebar :key="componentKey" :subItems="items[selectedIndex].subItems" />
     </aside>
-      <div v-if="dropdown" class="right-0 top-0 absolute mt-16 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+<!-- end second sidebar -->
+<!-- dropdown -->
+      <div v-if="dropdown" class="right-0 top-0 absolute mt-16 w-56 rounded-md shadow-lg bg-gray-100 ring-1 ring-black ring-opacity-5 focus:outline-none">
         <div class="py-1" >
-          <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-          <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
-          <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
-          <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
-          <form method="POST" action="#" role="none">
-            <button type="submit" class="text-gray-700 block w-full text-left px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-3">
-              Sign out
-            </button>
-          </form>
+    
+
+          <span class="flex items-center justify-start pl-4 ">
+            <span class="flex items-center justify-center">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            <a href="#" class="text-gray-700 block px-4 py-2 text-sm ">Search</a>
+            </span>
+          <span></span>
+          </span>
+
+          <span class="flex items-center justify-between px-4 hover:bg-gray-300">
+            <span class="flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+            </svg>
+            <a class="text-gray-700 block px-4 py-2 text-sm">Notification</a>
+            </span>
+          <span class="text-xs font-semibold inline-block py-1 px-2 rounded text-white bg-green-600 uppercase last:mr-0 mr-1">
+            10
+          </span>
+
+
+          </span>
+
+
+          <span class="flex items-center justify-start pl-4 hover:bg-gray-300">
+            <span class="flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
+            </svg>
+            <a href="#" class="text-gray-700 block px-4 py-2 text-sm">Message</a>
+            </span>
+            <span>
+            </span>           
+          </span>
+          
+          <span class="flex items-center justify-start pl-4 hover:bg-gray-300">
+            <span class="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            <a href="#" class="text-gray-700 block px-4 py-2 text-sm">Settings</a>
+            </span>
+            <span>
+              
+            </span>           
+          </span>
+
+          <span class="flex items-center justify-start pl-4 hover:bg-gray-300">
+            <span class="flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
+            <a href="#" class="text-gray-700 block px-4 py-2 text-sm">Profile</a>
+            </span>
+            <span>
+              
+            </span>           
+          </span>
+          
         </div>
       </div>
+
   </nav>
 
 
@@ -61,28 +114,48 @@
 <script>
 import Sidebar from './Sidebar1'
 import Content from './content.vue'
+import svg1 from '../assets/svgs/1.vue'
+
 export default {
   data() {
     return {
       items : [
         {
           name : 'Home 1',
+          svg : `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z" />
+                </svg>`, 
           subItems : [
             {name : 'Subitem 1'},{name : 'Subitem 2'},{name : 'Subitem 3'}
             ]
         },
         {
           name : 'Home 2',
-           subItems : [
+          svg : `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>`, 
+         subItems : [
             {name : '2 Subitem 1'},{name : '2 Subitem 2'},{name : '2 Subitem 3'}
           ]
         },
-        // {
-        //   name : 'Home 3',
-        //    subItems : [
-        //     {name : '3 Subitem 1'},{name : '3 Subitem 2'},{name : '3 Subitem 3',}
-        //   ]
-        // },
+        {
+          name : 'Home 4',
+          svg : `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>`,
+          subItems : [
+            {name : '4 Subitem 1'},{name : '4 Subitem 2'},{name : '4 Subitem 3',}
+          ]
+        },
+        {
+          name : 'Home 4',
+          svg : `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>`,
+          subItems : [
+            {name : '3 Subitem 1'},{name : '3 Subitem 2'},{name : '3 Subitem 3',}
+          ]
+        },
         ],
          componentKey: 0,
       isOpen: false,
@@ -134,8 +207,12 @@ export default {
   computed: {
   },
   components:{
-    Sidebar,
+    Sidebar, svg1,
     Content
 }
 };
 </script>
+
+<style scoped>
+
+</style>
--- components/Sidebar1.vue
+++ components/Sidebar1.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
-       <span v-for="(item, index) in lsubItems" :key="index" class="flex items-center p-4 hover:bg-indigo-500 hover:text-white ">
-           {{item.name}}
+       <span v-for="(item, index) in lsubItems" :key="index" class="flex items-end">
+          <div class="bg-gray-700 w-full py-2 px-1">
+      <div class="flex items-center"> 
+         <img alt="avatar" class="w-8 rounded-full " src="https://picsum.photos/seed/picsum/200" />
+         <div class="ml-1">
+            <p class="text-sm text-gray-100 font-semibold">{{item.name}}</p>
+            <p class="text-xs text-gray-400">3 open, 4 closed</p>
+         </div> 
+      </div>
+<!-- progress bar -->
+          <div class="w-full mt-1 bg-gray-100 rounded-full h-1.5 dark:bg-gray-700">
+            <div class="bg-green-300 h-1.5 rounded-full dark:bg-gray-300" style="width: 45%"></div>
+          </div>
+   </div>
         </span>         
     </div>
 
