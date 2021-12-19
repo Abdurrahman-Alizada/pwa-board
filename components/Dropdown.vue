09c742d32a51a077f2cd21d22097e1cb6181fa1c
Abdurrahman-Alizada <abdurrahmanalizada17@gmail.com>
2021-12-19 08:08:44 +0500

    final touches

---
+++ components/Dropdown.vue
@@ -0,0 +1,87 @@
+<template>
+         <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+    >
+        <div  class="right-0 top-0 absolute mt-16 w-56 rounded-md shadow-lg bg-gray-100 ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="py-1" >
+          <span class="flex items-center justify-start pl-4 ">
+            <span class="flex items-center justify-center">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            <a href="#" class="text-gray-700 block px-4 py-2 text-sm ">Search</a>
+            </span>
+          <span></span>
+          </span>
+
+          <span class="flex items-center justify-between px-4 hover:bg-gray-300">
+            <span class="flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+            </svg>
+            <a class="text-gray-700 block px-4 py-2 text-sm">Notification</a>
+            </span>
+          <span class="text-xs font-semibold inline-block py-1 px-2 rounded text-white bg-green-600 uppercase last:mr-0 mr-1">
+            10
+          </span>
+
+
+          </span>
+
+
+          <span class="flex items-center justify-start pl-4 hover:bg-gray-300">
+            <span class="flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
+            </svg>
+            <a href="#" class="text-gray-700 block px-4 py-2 text-sm">Message</a>
+            </span>
+            <span>
+            </span>           
+          </span>
+          
+          <span class="flex items-center justify-start pl-4 hover:bg-gray-300">
+            <span class="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            <a href="#" class="text-gray-700 block px-4 py-2 text-sm">Settings</a>
+            </span>
+            <span>
+              
+            </span>           
+          </span>
+
+          <span class="flex items-center justify-start pl-4 hover:bg-gray-300">
+            <span class="flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
+            <a href="#" class="text-gray-700 block px-4 py-2 text-sm">Profile</a>
+            </span>
+            <span>
+              
+            </span>           
+          </span>
+          
+        </div>   
+      </div>
+         </transition>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- components/Home.vue
+++ components/Home.vue
@@ -1,6 +1,7 @@
 <template>
+        
+  <nav @focusout="handleFocusOut" tabindex="0" class="flex fixed w-full items-center justify-between h-16 bg-white text-gray-700 border-b border-gray-200 z-10">
 <!-- navbar -->
-  <nav class="flex fixed w-full items-center justify-between h-16 bg-white text-gray-700 border-b border-gray-200 z-10">
        <div class="flex items-center justify-center">
         <button class="h-16 flex items-center justify-center w-16 border-r border-gray-400" aria-label="Open Menu" @click="drawer">
 
@@ -30,82 +31,15 @@
       </span>        
     </aside> 
 <!-- second sidebar -->
-    <aside class="transform mt-16 ml-16 top-0 left-0 w-36 bg-gray-700 border-r border-gray-300 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-0"  
+    <aside class="transform mt-16 ml-16 top-0 left-0 w-48 bg-gray-700 border-r border-gray-300 fixed h-full overflow-auto ease-in-out transition-all duration-700 z-0"  
      :class="isOpen ? 'translate-x-0' : '-translate-x-full'" 
      >
      <Sidebar :key="componentKey" :subItems="items[selectedIndex].subItems" />
     </aside>
-<!-- end second sidebar -->
-<!-- dropdown -->
-      <div v-if="dropdown" class="right-0 top-0 absolute mt-16 w-56 rounded-md shadow-lg bg-gray-100 ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <div class="py-1" >
-    
-
-          <span class="flex items-center justify-start pl-4 ">
-            <span class="flex items-center justify-center">
-            
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
-            <a href="#" class="text-gray-700 block px-4 py-2 text-sm ">Search</a>
-            </span>
-          <span></span>
-          </span>
-
-          <span class="flex items-center justify-between px-4 hover:bg-gray-300">
-            <span class="flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
-            <a class="text-gray-700 block px-4 py-2 text-sm">Notification</a>
-            </span>
-          <span class="text-xs font-semibold inline-block py-1 px-2 rounded text-white bg-green-600 uppercase last:mr-0 mr-1">
-            10
-          </span>
-
-
-          </span>
-
-
-          <span class="flex items-center justify-start pl-4 hover:bg-gray-300">
-            <span class="flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
-            </svg>
-            <a href="#" class="text-gray-700 block px-4 py-2 text-sm">Message</a>
-            </span>
-            <span>
-            </span>           
-          </span>
-          
-          <span class="flex items-center justify-start pl-4 hover:bg-gray-300">
-            <span class="flex items-center justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-            </svg>
-            <a href="#" class="text-gray-700 block px-4 py-2 text-sm">Settings</a>
-            </span>
-            <span>
-              
-            </span>           
-          </span>
-
-          <span class="flex items-center justify-start pl-4 hover:bg-gray-300">
-            <span class="flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-            </svg>
-            <a href="#" class="text-gray-700 block px-4 py-2 text-sm">Profile</a>
-            </span>
-            <span>
-              
-            </span>           
-          </span>
-          
-        </div>
-      </div>
-
+   <!-- end second sidebar -->
+   <!-- dropdown -->
+   
+   <Dropdown v-if="dropdown" />
   </nav>
 
 
@@ -113,8 +47,10 @@
 
 <script>
 import Sidebar from './Sidebar1'
+// import Dropdown from './Dropdown.vue'
 import Content from './content.vue'
 import svg1 from '../assets/svgs/1.vue'
+import Dropdown from './Dropdown.vue';
 
 export default {
   data() {
@@ -167,12 +103,23 @@ export default {
     drawer() {
       this.isOpen = !this.isOpen;
     },
+    clickOnIcon(){
+      this.isOpen = true;
+    },
     selectedItem(index){
       this.selectedIndex = index;
       console.log('in methods', index)
+      this.clickOnIcon()
     },
     showdropdown(){
       this.dropdown = !this.dropdown
+    },
+
+    handleFocusOut(){
+     this.isOpen = false
+    }, 
+    handleFocusOutDrop(){
+     this.dropdown = false
     }
   },
   watch: {
@@ -186,15 +133,13 @@ export default {
       }
     },
 
-        selectedIndex: {
+    selectedIndex: {
       immediate: true,
       handler(index) {
         if (process.client) {
          this.selectedIndex = index
          console.log('in watch', index)
          this.componentKey += 1;
-         // if (isOpen) document.body.style.setProperty("overflow", "hidden");
-          // else document.body.style.removeProperty("overflow");
         }
       }
     }
@@ -208,7 +153,8 @@ export default {
   },
   components:{
     Sidebar, svg1,
-    Content
+    Content,
+    Dropdown
 }
 };
 </script>
